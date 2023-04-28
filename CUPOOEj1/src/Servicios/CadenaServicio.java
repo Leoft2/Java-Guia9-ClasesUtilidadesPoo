752bb2f6a2160c926b2cc1ae2ead4eb81261e74d
Leoft2 <leonelvalenzuela110@gmail.com>
2023-04-27 22:00:33 -0300

    segundo commit

--- CUPOOEj1/src/Servicios/CadenaServicio.java
+++ CUPOOEj1/src/Servicios/CadenaServicio.java
@@ -22,22 +22,78 @@ public class CadenaServicio {
         Cadena c1 = new Cadena();
 
         System.out.println("Ingresar una palabra o frase");
-        c1.setFrase(leer.next());
+        c1.setFrase(leer.nextLine());
+        System.out.println("");
+
         return c1;
     }
 
-    public void MostrasVocales(Cadena c1) {
-        int contadorA= 0;
-        for (int i = 0; i < c1.getLongitudFrase(); i++) {
-            String frase = c1.getFrase();
-            String letra = frase.substring(i, i);
-            
-            switch (frase) {
-                case "a":
-                contadorA = contadorA++;
+    public void MostrarVocales(Cadena c1) {
+        String entrada = c1.getFrase();
+        int nVocales = 0;
+
+        for (int i = 0; i < entrada.length(); i++) {
+
+            if (entrada.charAt(i) == 'a' || entrada.charAt(i) == 'e' || entrada.charAt(i) == 'i'
+                    || entrada.charAt(i) == 'o' || entrada.charAt(i) == 'u') {
+                nVocales++;
             }
-           
         }
+        System.out.println("***Mostrar vocales***");
+        System.out.println("la frase " + entrada + " contiene " + nVocales + " vocales ");
+        System.out.println("");
+
     }
 
+    public void InvertirFrase(Cadena c1) {
+        String entrada = c1.getFrase();
+        System.out.println("***InvertirFrase***");
+        System.out.println("Entrada:" + entrada);
+        System.out.print("Salida:");
+        for (int i = entrada.length() - 1; i >= 0; i--) {
+            System.out.print(entrada.charAt(i));
+        }
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+    public void VecesRepetido(Cadena c1) {
+        Scanner leer = new Scanner(System.in);
+        String entrada = c1.getFrase();
+        System.out.println("***VecesRepetido***");
+        System.out.println("ingrese un caracter");
+        String letra = leer.next();
+        int nRepetido = 0;
+
+        for (int i = 0; i < entrada.length(); i++) {
+            String fraseLetra = entrada.substring(i, i + 1);
+            if (fraseLetra.equals(letra)) {
+                nRepetido++;
+            }
+        }
+        System.out.println("Entrada:" + entrada);
+        System.out.println("Salida: el caracter '" + letra + "' se repite " + nRepetido + " veces");
+        System.out.println("");
+    }
+
+    public void compararLongitud(Cadena c1) {
+        Scanner leer = new Scanner(System.in);
+        int longitudFrase1 = c1.getFrase().length();
+        System.out.println("***CompararLongitud***");
+        System.out.println("Ingresar una nueva frase:");
+        String frase2 = leer.nextLine();
+        int longitudFrase2 = frase2.length();
+        
+        if (longitudFrase1 == longitudFrase2) {
+            System.out.println("La longitud de las dos frases son iguales");
+            }
+            else {
+            System.out.println("La longitud de las dos frases son diferentes");
+            }
+        System.out.println("");
+    }
+    
+    
+    
 }
--- CUPOOEj1/src/cupooej1/ClaseString.java
+++ CUPOOEj1/src/cupooej1/ClaseString.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cupooej1;
 
+import Entidades.Cadena;
+import Servicios.CadenaServicio;
+import java.util.Scanner;
 
 public class ClaseString {
 
@@ -13,7 +15,44 @@ public class ClaseString {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic
+        Scanner leer = new Scanner(System.in);
+        CadenaServicio cs = new CadenaServicio();
+        Cadena c1 = cs.IngresarFrase();
+
+        System.out.println("");
+        System.out.println("---BIENVENIDO AL MENU DE METODOS DE LA CLASE STRING---");
+        System.out.println("1.Mostrar vocales");
+        System.out.println("2.Invertir frase");
+        System.out.println("3.Contar cuantas veces un x caracter se repite");
+        System.out.println("4.Comparar longitud con frase nueva");
+        System.out.println("X.Salir");
+        System.out.println("");
+        
+        System.out.println("Ingrese el numero de la opcion");
+        int opcion = leer.nextInt();
+        System.out.println("");
+        
+        switch (opcion) {
+            case 1:
+                cs.MostrarVocales(c1);
+                break;
+            case 2:
+                cs.InvertirFrase(c1);
+                break;
+            case 3:
+                cs.VecesRepetido(c1);
+                break;
+            case 4:
+                cs.compararLongitud(c1);
+                break;
+
+            default:
+                System.out.println("Opcion invalida -> SALIDA");
+
+        }
+        
+        
+        
     }
 
 }
